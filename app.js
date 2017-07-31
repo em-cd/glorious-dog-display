fdd1a663aa4932565530375b71216e050fdfeb9b
Lizzie Cullen Davison <lizzie.cullendavison@gmail.com>
2017-07-31 15:18:42 +0100

    added pagination, bootstrap and some custom styles, updated README

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-angular.module('dogApp', [])
+angular.module('dogApp', ['ui.bootstrap'])
   .controller('DogAppController', function($scope, DogService) {
+    
+    // set up pagination defaults
+    $scope.pagination = {
+      currentPage: 1,
+      numPerPage: 4
+    };
 
+    // fetch list of dog breeds using DogService for the http request
     $scope.getDogBreeds = function() {
       DogService.getDogBreeds().then(
         function(response) {
@@ -10,23 +17,34 @@ angular.module('dogApp', [])
         });
     };
 
+    // fetch array of image links for a certain breed
     $scope.getDogImages = function(breed) {
       DogService.getDogImages(breed).then(
         function(response) {
           $scope.dogImages = response.data.message;
+          $scope.currentImages = $scope.dogImages.slice(0, $scope.numPerPage);
         }).catch( function(errorResponse) {
           console.log('Error fetching dog images', errorResponse);
         });
-    }
+    };
 
-    $scope.getImages = function() {
+    // fetch new images and reset current page number when new breed type is selected
+    $scope.fetchImages = function() {
       $scope.getDogImages($scope.selectedBreed);
-    }
+      $scope.pagination.currentPage = 1;
+    };
 
+    // return a shorter array of images to display depending on the page number and default number of images per page
+    $scope.pageChanged = function() {
+      var begin = ($scope.pagination.currentPage - 1) * $scope.pagination.numPerPage;
+      var end = begin + $scope.pagination.numPerPage;
+      $scope.currentImages = $scope.dogImages.slice(begin, end);
+    };
+
+    // initialise app
     $scope.init = function() {
       $scope.getDogBreeds();
     };
-
     $scope.init();
  })
 
