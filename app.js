424cbae43c1f88af892e13978f55291380df4add
Lizzie Cullen Davison <lizzie.cullendavison@gmail.com>
2017-07-31 11:03:12 +0100

    added call to api to fetch dog breeds

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 angular.module('dogApp', [])
-  .controller('DogAppController', function($scope) {
-    $scope.dogBreedList = 
-      {
-        "status": "success",
-        "message": [
-          "affenpinscher",
-          "african",
-          "airedale",
-          "akita",
-          "appenzeller"]
-      };
- });
+  .controller('DogAppController', function($scope, DogService) {
+
+    $scope.getDogBreeds = function() {
+      DogService.getDogBreeds().then(
+        function(response) {
+          $scope.dogBreedList = response.data.message;
+        }).catch( function(errorResponse) {
+          console.log('Error loading list of dog breeds', errorResponse);
+        });
+    };
+
+    $scope.init = function() {
+      $scope.getDogBreeds();
+    };
+
+    $scope.init();
+ })
+
+  .factory('DogService', function($http) {
+    return {
+      getDogBreeds: function() {
+        return $http.get("https://dog.ceo/api/breeds/list");
+      }
+    }
+  });
